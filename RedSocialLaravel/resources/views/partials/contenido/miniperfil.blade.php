b6e5f174e1a58c9ee5c9dda392c282e480ccda5f
j0tac3 <j0s3.90jr@gmail.com>
2019-03-10 10:10:03 +0100

    ajajas

--- RedSocialLaravel/resources/views/partials/contenido/miniperfil.blade.php
+++ RedSocialLaravel/resources/views/partials/contenido/miniperfil.blade.php
@@ -1,3 +1,23 @@
 <div>
-    <img src="{{ asset('img/perfil.jpg') }}" title="" style="height: 50px" class="rounded-circle">
+    <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <img src="{{ asset('img/perfil.jpg') }}" title="" style="height: 50px" class="rounded-circle">
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="#">Ver perfil</a>
+            <a class="dropdown-item" href="#">Estado</a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item" href="#">Chats</a>
+            </div>
+        </li>
+        <li class="nav-item active">
+            <a class="nav-link" href="#">Mensajes <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">amigos</a>
+        </li>
+        </ul>
+    </div>
 </div>
