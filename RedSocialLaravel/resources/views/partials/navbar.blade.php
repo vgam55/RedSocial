598d11630e98373d102287e25b4ba948921f3177
j0tac3 <j0s3.90jr@gmail.com>
2019-04-01 00:07:54 +0200

    cambios

--- RedSocialLaravel/resources/views/partials/navbar.blade.php
+++ RedSocialLaravel/resources/views/partials/navbar.blade.php
@@ -1,10 +1,12 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark d-flex nav_bar">
     <a class="navbar-brand flex-grow-1" href=" {{ url('inicio') }}">SocialTX</a>
     <!-- Barra de busqueda -->
+    @if (Auth::check())
     <form class="form-inline my-2 my-lg-0" action="{{ url('/findAmigos') }}" method="GET">
         <input class="form-control mr-sm-2" type="input" id="buscar" name="buscar" placeholder="Buscar" aria-label="Search">
         <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Buscar</button>
     </form>
+    @endif
     <!-- Boton para cuando la pantalla se reduzca -->
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
