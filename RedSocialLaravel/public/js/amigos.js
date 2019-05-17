1e8d723fc7bc0246229d7d511e35d69759e6f234
victor <vgam55@gmail.com>
2019-05-17 11:17:05 +0200

    victor 17/05 v1

--- RedSocialLaravel/public/js/amigos.js
+++ RedSocialLaravel/public/js/amigos.js
@@ -42,9 +42,11 @@ $(function(){
 						for (var i=0;i<amigos.length;i++)
 						{
 							divAmigo=divAmigo+"<div>";
-							divAmigo=divAmigo+"<img src=\"img\\"+amigos[i]['avatar']+"\""+"/>";
-							divAmigo=divAmigo+"<p>"+amigos[i]['name']+"</p>";
+							divAmigo=divAmigo+"<img src=\"img\\"+amigos[i]['avatar']+"\""+"class='img-fluid'/>";
+							divAmigo=divAmigo+"<a href=\"{{ url('/deleteAmigos/'"+amigos[i]['id_Usuario']+") }}\" class='btn btn-danger stretched-link col-6'>"+"Eliminar"+"</a>";
+							divAmigo=divAmigo+"<a href='/deleteAmigo/'"+amigos[i]['id_usuario']+"' class='btn btn-primary stretched-link col-6'>"+amigos[i]["name"]+"</a>";
 							divAmigo=divAmigo+"</div>";
+							console.log("/deleteAmigo/"+amigos[i]['id_Usuario'])
 						}
 						friends.append(divAmigo);
 						
