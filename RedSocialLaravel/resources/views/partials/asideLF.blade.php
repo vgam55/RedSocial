3eb325a7952e39a51b8b630d64b295633cfaf19b
unknown <j0s3.90jr@gmail.com>
2019-05-22 19:45:16 +0200

    publicaciones

--- RedSocialLaravel/resources/views/partials/asideLF.blade.php
+++ RedSocialLaravel/resources/views/partials/asideLF.blade.php
@@ -1,9 +1,9 @@
-    <div class="row my-3 border border border-primary p-3">
+<div class="row my-3 p-3">
         @include('partials.contenido.miniperfil')
     </div>
-    <div class="row my-5 border border border-primary p-5">
+    <div class="row my-5  p-5">
         @include('partials.contenido.alertas')
     </div>
-    <div class="row my-5 border border border-primary p-5">
+    <div class="row my-5  p-5">
         @include('partials.contenido.calendario')
     </div>
--- RedSocialLaravel/resources/views/partials/asideRG.blade.php
+++ RedSocialLaravel/resources/views/partials/asideRG.blade.php
@@ -1,6 +1,4 @@
-    <div class="row my-5 border border border-primary p-3" id="amigos" name="amigos">
-        @include('partials.contenido.amigos')       
-    </div>
-    <div class="row my-5 border border border-primary p-3" id="chat" name="chat">
-        @include('partials.contenido.chat')
-    </div>
+<div class="row my-5 p-3" id="amigos" name="amigos">
+    @include('partials.contenido.amigos')
+</div>
+
