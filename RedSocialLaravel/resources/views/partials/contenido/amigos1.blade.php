146dcd5eb80a3331b23b919207a5c334e4581672
Sierra Berganzo <BISIBEAN@intranet.ibermatica>
2019-05-27 09:43:43 +0200

    publicaciones

--- RedSocialLaravel/resources/views/partials/contenido/amigos1.blade.php
+++ RedSocialLaravel/resources/views/partials/contenido/amigos1.blade.php
@@ -2,15 +2,15 @@
 @section('adiskideak')
 
 	@foreach($listaAmigos as $amigo)
-	<div >
-	 
+	<div ><br>
+
 	  <img src="{{ asset('img/'.$amigo->avatar) }}" alt="{{$amigo->avatar}}" class="img-fluid">
-	 
-	  <form class="form-inline" action="{{ url('/deleteAmigos/'.$amigo->id_Usuario_Amigo) }}" method="POST">
+
+	  <form class="form-inline text-center d-flex" action="{{ url('/deleteAmigos/'.$amigo->id_Usuario_Amigo) }}" method="POST">
 	    {{ csrf_field() }}
 	    {{ method_field('DELETE') }}
-        <a href="{{url('/getPublicaciones/'.$amigo->id_usuario_destinatario)}}" class="btn btn-primary stretched-link col-5">{{$amigo->name}}</a>
-	    <button class="btn btn-danger stretched-link col-5" type="submit">Borrar</button>
+        <a href="{{url('/getPublicaciones/'.$amigo->id_usuario_destinatario)}}" class="btn btn-primary btn-sm stretched-link flex-fill">{{$amigo->name}}</a>
+	    <button class="btn btn-danger btn-sm stretched-link flex-fill" type="submit">Borrar</button>
 	  </form>
 	</div>
 	@endforeach
